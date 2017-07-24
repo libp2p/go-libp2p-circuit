a364d0349636dc42a1b6fea5baa24ada808b6560
vyzo <vyzo@hackzen.org>
2017-07-24 14:15:55 +0300

    refactor transport test

--- transport_test.go
+++ transport_test.go
@@ -18,10 +18,9 @@ import (
 
 const TestProto = "test/relay-transport"
 
-func TestRelayTransport(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+var msg = []byte("relay works!")
 
+func testSetupRelay(t *testing.T, ctx context.Context) []host.Host {
 	hosts := getNetHosts(t, ctx, 3)
 
 	err := AddRelayTransport(ctx, hosts[0])
@@ -44,7 +43,6 @@ func TestRelayTransport(t *testing.T) {
 
 	time.Sleep(100 * time.Millisecond)
 
-	msg := []byte("relay works!")
 	handler := func(s inet.Stream) {
 		_, err := s.Write(msg)
 		if err != nil {
@@ -55,18 +53,21 @@ func TestRelayTransport(t *testing.T) {
 
 	hosts[2].SetStreamHandler(TestProto, handler)
 
-	testFullAddressDial(t, hosts, msg)
-	testSpecificRelayDial(t, hosts, msg)
-	testUnspecificRelayDial(t, hosts, msg)
+	return hosts
 }
 
-func testFullAddressDial(t *testing.T, hosts []host.Host, msg []byte) {
+func TestFullAddressTransportDial(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	hosts := testSetupRelay(t, ctx)
+
 	addr, err := ma.NewMultiaddr(fmt.Sprintf("%s/ipfs/%s/p2p-circuit/ipfs/%s", hosts[1].Addrs()[0].String(), hosts[1].ID().Pretty(), hosts[2].ID().Pretty()))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rctx, rcancel := context.WithTimeout(context.Background(), time.Second)
+	rctx, rcancel := context.WithTimeout(ctx, time.Second)
 	defer rcancel()
 
 	hosts[0].Peerstore().AddAddrs(hosts[2].ID(), []ma.Multiaddr{addr}, pstore.TempAddrTTL)
@@ -86,13 +87,18 @@ func testFullAddressDial(t *testing.T, hosts []host.Host, msg []byte) {
 	}
 }
 
-func testSpecificRelayDial(t *testing.T, hosts []host.Host, msg []byte) {
+func TestSpecificRelayTransportDial(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	hosts := testSetupRelay(t, ctx)
+
 	addr, err := ma.NewMultiaddr(fmt.Sprintf("/ipfs/%s/p2p-circuit/ipfs/%s", hosts[1].ID().Pretty(), hosts[2].ID().Pretty()))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rctx, rcancel := context.WithTimeout(context.Background(), time.Second)
+	rctx, rcancel := context.WithTimeout(ctx, time.Second)
 	defer rcancel()
 
 	hosts[0].Peerstore().AddAddrs(hosts[2].ID(), []ma.Multiaddr{addr}, pstore.TempAddrTTL)
@@ -112,13 +118,18 @@ func testSpecificRelayDial(t *testing.T, hosts []host.Host, msg []byte) {
 	}
 }
 
-func testUnspecificRelayDial(t *testing.T, hosts []host.Host, msg []byte) {
+func TestUnspecificRelayTransportDial(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	hosts := testSetupRelay(t, ctx)
+
 	addr, err := ma.NewMultiaddr(fmt.Sprintf("/p2p-circuit/ipfs/%s", hosts[2].ID().Pretty()))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rctx, rcancel := context.WithTimeout(context.Background(), time.Second)
+	rctx, rcancel := context.WithTimeout(ctx, time.Second)
 	defer rcancel()
 
 	hosts[0].Peerstore().AddAddrs(hosts[2].ID(), []ma.Multiaddr{addr}, pstore.TempAddrTTL)
