5e1217a5a09dcc7fbe056d28d48d28fcfb40008d
vyzo <vyzo@hackzen.org>
2021-02-05 17:36:12 +0200

    implement missing details

--- v2/relay/resources.go
+++ v2/relay/resources.go
@@ -7,7 +7,9 @@ import (
 type Resources struct {
 	Limit *RelayLimit
 
-	ReservationTTL  time.Duration
+	ReservationTTL        time.Duration
+	ReservationRefreshTTL time.Duration
+
 	MaxReservations int
 	MaxCircuits     int
 	BufferSize      int
@@ -16,4 +18,24 @@ type Resources struct {
 type RelayLimit struct {
 	Duration time.Duration
 	Data     int64
+}
+
+func DefaultResources() Resources {
+	return Resources{
+		Limit: DefaultLimit(),
+
+		ReservationTTL:        time.Hour,
+		ReservationRefreshTTL: 15 * time.Minute,
+
+		MaxReservations: 1024,
+		MaxCircuits:     16,
+		BufferSize:      2048,
+	}
+}
+
+func DefaultLimit() *RelayLimit {
+	return &RelayLimit{
+		Duration: time.Minute,
+		Data:     65536,
+	}
 }
