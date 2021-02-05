ff9fdfc721482fd2a5da7a46106aa1876a08618c
vyzo <vyzo@hackzen.org>
2021-02-05 17:38:33 +0200

    add options for resources/limit

--- v2/relay/options.go
+++ v2/relay/options.go
@@ -1,3 +1,17 @@
 package relay
 
 type Option func(*Relay) error
+
+func WithResources(rc Resources) Option {
+	return func(r *Relay) error {
+		r.rc = rc
+		return nil
+	}
+}
+
+func WithLimit(limit *RelayLimit) Option {
+	return func(r *Relay) error {
+		r.rc.Limit = limit
+		return nil
+	}
+}
