e7e7bf16facd1adfad9d2f703f6fb40e2ee3e57a
vyzo <vyzo@hackzen.org>
2021-02-12 19:34:23 +0200

    godocs

--- v2/relay/options.go
+++ v2/relay/options.go
@@ -4,6 +4,7 @@ import ()
 
 type Option func(*Relay) error
 
+// WithResources is a Relay option that sets specific relay resources for the relay.
 func WithResources(rc Resources) Option {
 	return func(r *Relay) error {
 		r.rc = rc
@@ -11,6 +12,7 @@ func WithResources(rc Resources) Option {
 	}
 }
 
+// WithLimit is a Relay option that sets only the relayed connection limits for the relay.
 func WithLimit(limit *RelayLimit) Option {
 	return func(r *Relay) error {
 		r.rc.Limit = limit
