25af30386c9a6aaeea3c8f7a42610bba44cf669b
Marten Seemann <martenseemann@gmail.com>
2022-01-18 00:12:54 -0800

    chore: update go-log to v2 (#147)

--- relay.go
+++ relay.go
@@ -16,8 +16,10 @@ import (
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	"github.com/libp2p/go-libp2p-core/transport"
 
-	logging "github.com/ipfs/go-log"
 	pool "github.com/libp2p/go-buffer-pool"
+
+	logging "github.com/ipfs/go-log/v2"
+
 	ma "github.com/multiformats/go-multiaddr"
 )
 
